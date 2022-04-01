646439f2235c7f61a0c10a0f8e19a378ee4d589d
xinyun <xinyun.fang@ucdconnect.ie>
2022-04-01 02:22:25 +0100

    gaussian

--- WaterColor/shaders/boardFS.glsl
+++ WaterColor/shaders/boardFS.glsl
@@ -8,50 +8,63 @@ uniform sampler2D edgeTexture;
 uniform int Width = 800; // The pixel width
 uniform int Height = 600; // The pixel height
 
-uniform float sigma = 20;
+uniform float sigma = 5;
 const float pi = 3.14159265f;
-const float numBlurPixelsPerSide = 10.0f;
+const float numBlurPixelsPerSide = 7.0f;
 const vec2 blurMultiplyVecV = vec2(0.0f, 1.0f);
 const vec2 blurMultiplyVecH = vec2(1.0f, 0.0f);
-float blurSize = 1.0 / float(Height);
+
+uniform float edgethreshold = 0.1;
 void main()
 {
     //gl_FragColor = texture(edgeTexture, TexCoord);
     //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 
-
     float dx = 1.0 / float(Width);
     float dy = 1.0 / float(Height);
 
 
-    // Incremental Gaussian Coefficent Calculation (See GPU Gems 3 pp. 877 - 889)
-    vec3 incrementalGaussian;
-    incrementalGaussian.x = 1.0f / (sqrt(2.0f * pi) * sigma);
-    incrementalGaussian.y = exp(-0.5f / (sigma * sigma));
-    incrementalGaussian.z = incrementalGaussian.y * incrementalGaussian.y;
+    float denominator= 2.0f * pi * sigma * sigma;
+    float numerator = exp(0);
 
     vec4 avgValue = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-    float coefficientSum = 0.0f;
-
-    // Take the central sample first...
-    avgValue += texture2D(edgeTexture, TexCoord.st) * incrementalGaussian.x;
-    coefficientSum += incrementalGaussian.x;
-    incrementalGaussian.xy *= incrementalGaussian.yz;
-
-    // Go through the remaining 8 vertical samples (4 on each side of the center)
-    for (float i = 1.0f; i <= numBlurPixelsPerSide; i++) { 
-        avgValue += texture2D(edgeTexture, TexCoord.st - i * dy * blurMultiplyVecV) * incrementalGaussian.x;         
-        avgValue += texture2D(edgeTexture, TexCoord.st + i * dy * blurMultiplyVecV) * incrementalGaussian.x;
-        avgValue += texture2D(edgeTexture, TexCoord.st - i * dx * blurMultiplyVecH) * incrementalGaussian.x;         
-        avgValue += texture2D(edgeTexture, TexCoord.st + i * dx * blurMultiplyVecH) * incrementalGaussian.x;            
-        coefficientSum += 4 * incrementalGaussian.x;
-        incrementalGaussian.xy *= incrementalGaussian.yz;
-    }
-
     
+    //gl_FragColor = avgValue * 100;
+    for (float i = 1.0f; i <= numBlurPixelsPerSide; i++) { 
+        for(float j = 1.0f; j <= numBlurPixelsPerSide; j++){
+            float numerator = exp(-(i * i + j * j) / (2 * sigma * sigma));
+            vec2 offsets[4] = vec2[](
+                vec2( i * dx,  j * dy),
+                vec2(-i * dx,  j * dy),
+                vec2( i * dx, -j * dy),
+                vec2(-i * dx, -j * dy)
+            );
+            for(int k = 0; k < 4; k++){
+                avgValue += texture2D(edgeTexture, TexCoord.st + offsets[k]) * numerator / denominator;
+            }
+        }
+    }
+    for(int i = 0; i < numBlurPixelsPerSide; i++){
+        float numerator = exp(-(i * i) / (2 * sigma * sigma));
+        vec2 offsets[4] = vec2[](
+            vec2( i * dx, 0),
+            vec2(-i * dx, 0),
+            vec2(0,  i * dy),
+            vec2(0, -i * dy)
+        );
+        for(int k = 0; k < 4; k++){
+            avgValue += texture2D(edgeTexture, TexCoord.st + offsets[k]) * numerator / denominator;
+        }
+    }
+    //avgValue = avgValue - 3 * texture2D(edgeTexture, TexCoord) * numerator / denominator;
 
-    gl_FragColor = avgValue / coefficientSum;
-    //gl_FragColor = avgValue;
+    gl_FragColor = avgValue * 10;
+
+    //vec4 result = texture(objectTexture, TexCoord);
+    //result.a = 1 - step(edgethreshold, max(max(avgValue.x, avgValue.y), avgValue.z) * 100);
+
+
+    //gl_FragColor = vec4(vec3(step(0.1, max(max(avgValue.x, avgValue.y), avgValue.z) * 10)), 1.0);
 
     //gl_FragColor = normalize(abs(avgValue));
     //avgValue = avgValue / (avgValue + vec4(1.0));
